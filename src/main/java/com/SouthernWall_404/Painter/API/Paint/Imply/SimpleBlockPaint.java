419158796c1a010c59659b4f5754e03bc80d58a5
南墙客 <2396152847@qq.com>
2026-04-13 22:50:59 +0800

    2.0.12更新，移除了SimpleBlockPaint中的废旧方法

--- src/main/java/com/SouthernWall_404/Painter/API/Paint/Imply/SimpleBlockPaint.java
+++ src/main/java/com/SouthernWall_404/Painter/API/Paint/Imply/SimpleBlockPaint.java
@@ -60,80 +60,10 @@ public class SimpleBlockPaint extends AbstractPaint {
 
             Direction direction=getDirection(flag);
 
-            //            Block block= RenderUtil.getBlockFromID(paintPaths.get(flag));
             List<BakedQuad> quads = getQuadsForDirection(material,direction);
 
             objects.put(flag, quads);
 
         }
     }
-//
-//    @Override
-//    public void render(BlockEntity blockEntity, BlockPos blockPos, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay, float partialTick) {
-////        update();
-//        if (origin == null) return;
-//
-//        Level level = blockEntity.getLevel();
-//        if (level == null) return;
-//        BlockPos pos = blockEntity.getBlockPos();
-//
-//        // 对Origin的处理
-//        BakedModel model = Minecraft.getInstance().getModelManager().getBlockModelShaper().getBlockModel(origin);
-//        BlockColors blockColors = Minecraft.getInstance().getBlockColors();
-//        RandomSource random = RandomSource.create();
-//        long seed = origin.getSeed(pos);
-//        RenderType renderType = RenderUtil.getRenderType(origin);
-//
-//
-//
-//        // 创建自定义渲染器
-//        ModModelRender modRenderer = new ModModelRender(blockColors);
-//
-//        Vec3 offset = origin.getOffset(level, pos);
-//        poseStack.pushPose();
-//        poseStack.translate(offset.x, offset.y, offset.z);
-//
-//        // 准备 AO 计算所需的共享数组
-//        float[] shape = new float[ModModelRender.DIRECTIONS.length * 2];
-//        BitSet shapeFlags = new BitSet(3);
-//        ModModelRender.AmbientOcclusionFace aoFace = new ModModelRender.AmbientOcclusionFace();
-//
-//
-//        // 渲染每个方向的原版面（跳过有自定义的方向）
-//        for (Direction direction : Direction.values()) {
-//            List<BakedQuad> quads;
-//
-//            int flag=getFlag(direction);
-//            BlockPos neighborPos = pos.relative(direction);
-//            if (!RenderUtil.shouldRenderFace(origin, level, pos, direction, neighborPos)) continue;
-//
-//            if (objects.containsKey(flag)) {
-//                quads=objects.get(flag);
-//
-//                BlockState state= materials.get(flag);
-//                VertexConsumer consumer = bufferSource.getBuffer(RenderUtil.getRenderType(state));
-//
-//                renderQuadsWithAO(level,state,pos,quads,consumer,modRenderer,shape,shapeFlags,aoFace,poseStack,packedOverlay);
-//
-//            }
-//            else
-//            {
-//                VertexConsumer consumer = bufferSource.getBuffer(renderType);
-//                random.setSeed(seed);
-//                quads= model.getQuads(origin, direction, random, ModelData.EMPTY, renderType);
-//
-//                renderQuadsWithAO(level,origin,pos,quads,consumer,modRenderer,shape,shapeFlags,aoFace,poseStack,packedOverlay);
-//                // 渲染无方向的原版面（如粒子面）
-//                random.setSeed(seed);
-//                List<BakedQuad> generalQuads = model.getQuads(origin, null, random, ModelData.EMPTY, renderType);
-//                if (!generalQuads.isEmpty()) {
-//
-//                    renderQuadsWithAO(level,origin,pos,quads,consumer,modRenderer,shape,shapeFlags,aoFace,poseStack,packedOverlay);
-//                }
-//            }
-//            if (quads.isEmpty()) continue;
-//
-//        }
-//        poseStack.popPose();
-//    }
 }
