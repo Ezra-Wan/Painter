e9651dd611737c65e058d6eeda441c2729b54746
南墙客 <2396152847@qq.com>
2026-04-23 21:29:38 +0800

    2.3.15更新，添加了isValidPaintOperation方法，用于检测喷涂行为合法性

--- src/main/java/com/SouthernWall_404/Painter/API/Paint/Util/Paint/PaintValidHelper.java
+++ src/main/java/com/SouthernWall_404/Painter/API/Paint/Util/Paint/PaintValidHelper.java
@@ -1,9 +1,13 @@
 package com.SouthernWall_404.Painter.API.Paint.Util.Paint;
 
+import com.SouthernWall_404.LaplaceAPI.VertinCore.Util.BlockUtil;
 import com.SouthernWall_404.Painter.API.Paint.API.AbstractPaint;
 import com.SouthernWall_404.Painter.API.Paint.API.AbstractRender;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -37,13 +41,34 @@ public class PaintValidHelper {
     public static boolean isPaintable(BlockState origin,Level level,BlockPos blockPos)
     {
 
-        if (origin.isCollisionShapeFullBlock(level,blockPos))
+        if (BlockUtil.isFullBlock(level,blockPos,origin))
         {
             return true;//完整方块可行
         }
         if(origin.getBlock() instanceof SlabBlock)
         {
             return true;//半砖可行
+        }
+
+        return false;
+    }
+
+    /**
+     * 用于检验是否为合法的喷涂行动
+     * @param blockToPaint
+     * @param level
+     * @param blockPos
+     * @param blockToBePainted
+     * @return
+     */
+    public static boolean isValidPaintOperation(BlockState blockToPaint,Level level,BlockPos blockPos,BlockState blockToBePainted)
+    {
+        if(BlockUtil.isFullBlock(level,blockPos,blockToPaint))
+        {
+            if(isPaintable(blockToBePainted,level,blockPos))
+            {
+                return true;
+            }
         }
 
         return false;
