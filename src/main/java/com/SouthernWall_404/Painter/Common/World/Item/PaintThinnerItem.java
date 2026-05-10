368ae57121f936b2fd67254cf79e1b79e1f75c26
南墙客 <2396152847@qq.com>
2026-05-10 21:58:12 +0800

    feat(paint): 添加涂料稀释剂物品和单点材质移除功能
    - 在AbstractPaint中添加removeMaterial方法用于移除指定方向的材质
    - 添加getMaterials方法用于获取所有材质的映射
    - 新增PaintThinnerItem类作为涂料稀释剂物品
    - 在ModItems中注册PAINT_THINNER物品
    - 在PaintInfo中添加removePaintMaterial方法用于移除绘制材质
    - 在PaintOperationHelper中添加thinPaint方法支持单点和批量材质移除
    - 添加ChunkPos导入和相关集合类型声明

---
+++ src/main/java/com/SouthernWall_404/Painter/Common/World/Item/PaintThinnerItem.java
@@ -0,0 +1,39 @@
+package com.SouthernWall_404.Painter.Common.World.Item;
+
+import com.SouthernWall_404.Painter.API.Paint.Util.Paint.PaintOperationHelper;
+import com.SouthernWall_404.Painter.API.Tool.SelectedZone;
+import com.SouthernWall_404.Painter.Common.Init.ModAttachments;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+
+public class PaintThinnerItem extends BlockInteractItem{
+
+    public PaintThinnerItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public void dealLeftClick(PlayerInteractEvent.LeftClickBlock event) {
+
+    }
+
+    @Override
+    public void dealRightClick(PlayerInteractEvent.RightClickBlock event, boolean isInMainHand) {
+        Player player = event.getEntity();
+        SelectedZone zone = player.getData(ModAttachments.SELECTED_ZONE);
+        Level level = event.getLevel();
+        BlockPos pos = event.getPos();
+
+        if (zone.isSelecting()) {
+            // 执行大面积移除操作
+            // TODO 待实现
+        } else {
+            // 执行单点移除操作
+            PaintOperationHelper.thinPaint(level, pos,event.getFace());
+
+
+        }
+    }
+}
